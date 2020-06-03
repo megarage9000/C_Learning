5b0199e5ac3cb73452fcb01abc4f80763bbb1305
John Ordoyo <gtom_1984@hotmail.ca>
2020-06-03 16:42:41 -0700

    C Input / Output
    - Lesson Link: https://www.programiz.com/c-programming/c-input-output
    - Note: scanf isn't always the best to do input!

--- main.c
+++ main.c
@@ -1,63 +1,61 @@
 #include <stdio.h>
 
-// Lesson Link: 
-// https://www.programiz.com/c-programming/c-data-types
+// Input / Output
 
-void dataTypesDemo();
+// Lesson Link: 
+// https://www.programiz.com/c-programming/c-input-output
+
+// ScanF tips! (It's not always safe!):
+// http://sekrit.de/webdocs/c/beginners-guide-away-from-scanf.html
+
+void outputDemo();
+void inputDemo();
 
 int main(){
 
-    dataTypesDemo();
-    return -1;
+    // outputDemo();
+    inputDemo();
+    return 0;
 }
 
-void dataTypesDemo(){
+void outputDemo(){
 
-    // Integers
-    // Format: "%d"
-    int integerOne, integerTwo;
-    integerOne = 1;
-    integerTwo = -1;
-    printf("%s%d\n", "integerOne = ", integerOne);
-    printf("%s%d\n", "integerTwo = ", integerTwo);
+    printf(" ---- Output Demo ---- \n");
+    printf("String output example!\n");
 
-    // Floats and Doubles 
-    // Formatters:
-    // - float = "%f"
-    // - double = "%lf"
-    float fraction;
-    double price;
-    fraction = 1/4;
-    price = 22.95;
-    printf("%s%f\n", "fraction = ", fraction);
-    printf("%s%lf\n", "price = ", price);
-    
-    // Chars
-    // Format: "%c"
-    char letterA = 'A';
-    printf("%s%c\n", "letterA = ", letterA);
-    
+    int sampleInteger = 2;
+    printf("Sample integer = %d\n", sampleInteger);
 
-    // Shorts and Longs
-    // Formatters:
-    // - short = "%hd"
-    // - long = "%ld"
-    // - long long = "%lld"
-    // - long double = "%Lf"
-    short smallNumber = 22; 
-    long bigNumber = 222;
-    long long biggerNumber = 2222;
-    long double bigDoubleNumber = 22222;
-    printf("%s%hd\n", "smallNumber = ", smallNumber);
-    printf("%s%ld\n", "bigNumber = ", bigNumber);
-    printf("%s%lld\n", "biggerNumber = ", biggerNumber);
-    printf("%s%Lf\n", "bigDoubleNumber = ", bigDoubleNumber);
-    
+    float sampleFloat = 2.2;
+    double sampleDouble = 33.45;
+    printf("Sample float = %f\n", sampleFloat);
+    printf("Sample double = %lf\n", sampleDouble);
 
-    // Unsigned and Signed
-    // Format: "%u"
-    unsigned int nonNegativeInt = 24;
-    printf("%s%u\n", "nonNegativeInt = ", nonNegativeInt);
-
+    char characterA = 'A';
+    printf("Sample character = %c\n", characterA);
 }
 
+void inputDemo(){
+    printf(" ---- Input Demo ---- \n");
+
+    // Note for scanf, you must enter the correct formatter and 
+    // the memory location of the variable to store!
+
+    int integerInput;
+    printf("Enter an integer: ");
+    scanf("%d", &integerInput);
+    printf("Your entered integer = %d\n", integerInput);
+
+    float floatInput;
+    double doubleInput;
+    printf("Enter a float, and then a double: ");
+    scanf("%f%lf", &floatInput, &doubleInput);
+    printf("You have entered float = %f, and double = %lf\n", floatInput, doubleInput);
+
+    // Bugged area over here!
+    char characterInput;
+    printf("Enter a character: ");
+    scanf("%c", &characterInput);
+    printf("You have entered character = %c\n", characterInput);
+
+}
