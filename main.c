59a5da822b920c1f9a70a28b1a5e0ddb1c25855f
John Ordoyo <gtom_1984@hotmail.ca>
2020-06-08 16:33:47 -0700

    Update master
    - Added function to take in integer input
    - Added some libraries

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
+double getNumberInput();
 
 int main(){
     printf("%s\n", "How are you?");
     return -1;
 }
 
+double getNumberInput(){
+    int success = 0;
+    double result;
+    char buf[40];
+    do{
+        printf("Enter a number: ");
+        if(fgets(buf, sizeof(buf), stdin)){
+            char *endptr;
+            int base = 10;
+            errno = 0;
+            result = strtol(buf, &endptr, base);
+
+            int ifNoRead = (endptr == buf);
+            int ifEndlessRead = (*endptr && *endptr != '\n');
+
+            if(errno == ERANGE){
+                success = 0;
+            }
+            else if(ifNoRead || ifEndlessRead){
+                success = 0;
+            }
+            else{
+                success = 1;
+            }
+
+        } else{
+            return -1;
+        }
+    } while (!success);
+
+    return result;
+}
