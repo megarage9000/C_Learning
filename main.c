a8138cc65246c618475da45b7adf965254ac26a6
John Ordoyo <gtom_1984@hotmail.ca>
2020-06-07 22:35:27 -0700

    CLearning For loop
    - Lesson Link: https://www.programiz.com/c-programming/c-for-loop

--- main.c
+++ main.c
@@ -4,55 +4,54 @@
 #include <string.h>
 
 // Lesson Links: 
-// - Do/while loops: https://www.programiz.com/c-programming/c-do-while-loops
-// - If/Else statements: https://www.programiz.com/c-programming/c-if-else-statement
-void ifDemo();
+// - For Loops: https://www.programiz.com/c-programming/c-for-loop
+
+void forLoopDemo();
 
 int main(){
-    ifDemo();
+    forLoopDemo();
     return -1;
 }
 
-void ifDemo(){
+void forLoopDemo(){
 
-    // For boolean expressions:
-    // 1 == true, 0 == false
-    
     int success = 0;
-    int result;
+    double result;
     char buf[40];
-    do {
-        printf("What is 54 x 5 equal to? ");
-        if(fgets(buf, 40, stdin)){
-
+    do{
+        printf("Enter a number to multiply itself: ");
+        if(fgets(buf, sizeof(buf), stdin)){
             char *endptr;
             int base = 10;
             errno = 0;
             result = strtol(buf, &endptr, base);
 
-            int ifUnsuccessfulRead = (endptr == buf);
-            int ifNoEnd = (*endptr && *endptr != '\n');
+            int ifNoRead = (endptr == buf);
+            int ifEndlessRead = (*endptr && *endptr != '\n');
+
             if(errno == ERANGE){
                 success = 0;
             }
-            else if(ifUnsuccessfulRead == 1 || ifNoEnd == 1){
+            else if(ifNoRead || ifEndlessRead){
                 success = 0;
             }
             else{
                 success = 1;
             }
-        }
-        else{
+
+        } else{
             return;
         }
     } while (!success);
 
-    if(result == 270){
-        printf("success!\n");
+    for (int i = 0; i < 3; i++){
+        result *= result;
     }
-    else{
-        printf("wrong answer!\n");
-    }
+
+    printf("Your number multiplied by itself 3 times: %lf\n", result);
+
 }
+
+
 
 
