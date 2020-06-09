9e647f55754472884cc05e0dddda54b6fef1d2cd
John Ordoyo <gtom_1984@hotmail.ca>
2020-06-08 17:05:28 -0700

    CLearning User Functions
    - Lesson Links:
    	User Defined Functions: https://www.programiz.com/c-programming/c-user-defined-functions
    	Types of Functions: https://www.programiz.com/c-programming/types-user-defined-functions

--- main.c
+++ main.c
@@ -3,19 +3,42 @@
 #include <stdlib.h>
 #include <errno.h>
 
+// Lesson Links:
+// C User-Defined Functions: https://www.programiz.com/c-programming/c-user-defined-functions
+// C Function Types: https://www.programiz.com/c-programming/types-user-defined-functions
+
 double getNumberInput();
+void swapNumbers(int * a, int * b);
+void functionDemo();
 
 int main(){
-    printf("%s\n", "How are you?");
+    functionDemo();
     return -1;
 }
+
+void swapNumbers(int * a, int * b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void functionDemo(){
+    printf("Enter a number: ");
+    int a = (int)getNumberInput();
+    printf("Enter a number to swap the first one with: ");
+    int b = (int)getNumberInput();
+
+    printf("Before swapping: A = %d, B = %d\n", a, b);
+    swapNumbers(&a, &b);
+    printf("After swapping: A = %d, B = %d\n", a, b);
+}
+
 
 double getNumberInput(){
     int success = 0;
     double result;
     char buf[40];
     do{
-        printf("Enter a number: ");
         if(fgets(buf, sizeof(buf), stdin)){
             char *endptr;
             int base = 10;
