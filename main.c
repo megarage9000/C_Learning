526885b7534fa4ad109ff06d6f81df8ee430c69a
John Ordoyo <gtom_1984@hotmail.ca>
2020-06-13 18:29:02 -0700

    CLearning Strings and String Functions
    Lesson Links:
    - Strings: https://www.programiz.com/c-programming/c-strings
    - String Functions: https://www.programiz.com/c-programming/library-function/string.h/strlen

--- main.c
+++ main.c
@@ -3,11 +3,55 @@
 #include <stdlib.h>
 #include <errno.h>
 
+// CLearning Strings and String functions
+// - Lesson Links: 
+//      - https://www.programiz.com/c-programming/c-strings
+//      - https://www.programiz.com/c-programming/string-handling-functions
+
 double getNumberInput();
+char * getStringInput(int charLimit, char message[]);
+void freeString(char ** string);
+void stringDemo();
+
+const char answer[] = "John Ordoyo";
 
 int main(){
-    printf("%s\n", "How are you?");
+    stringDemo();
     return -1;
+}
+
+char * getStringInput(int charLimit, char message[]){
+    int success = 0;
+    char buf[charLimit];
+    char * result = NULL;
+    
+    printf("%s", message);
+    if(fgets(buf, charLimit, stdin)){
+        buf[strcspn(buf, "\n")] = 0;
+    }
+    
+    result = (char *)malloc(charLimit * sizeof(char));
+    strcpy(result, buf);
+    return result;
+}
+
+void freeString(char ** string){
+    free(*string);
+}
+
+void stringDemo(){
+    int success = 0;
+    do{
+        char * guess = getStringInput(100, "Guess my name: ");
+        int result = strcmp(answer, guess);
+        success = (result == 0);
+        if(!success){
+            printf("Wrong guess try again!\n");
+        }
+        freeString(&guess);
+
+    } while (!success);
+    printf("You guess my name! it was %s!\n", answer);
 }
 
 double getNumberInput(){
